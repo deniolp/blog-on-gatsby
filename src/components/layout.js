1151f47128e29f261b0ad19aaac1517d67158080
popov7 <deniolp@gmail.com>
2019-08-01 11:28:29 +0300

    Adjusting some styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -62,7 +62,11 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
-        <footer>
+        <footer
+          style={{
+            marginTop: 15,
+          }}
+        >
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
